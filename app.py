4ed90129c1c4fe15123507bfbe87991a6dad421b
Leo Carrera <leocarr315@gmail.com>
2026-04-09 11:49:33 -0400

    Initial commit: Add Flask web app for number addition

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        number1 = int(request.form['number1'])
+        number2 = int(request.form['number2'])
+        result = number1 + number2
+        return render_template('index.html', result=result)
+    return render_template('index.html', result=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
